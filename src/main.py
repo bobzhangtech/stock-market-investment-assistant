16d7c226576a0865764992bc9619bd6382fc5ec4
Bob Zhang <270489537+bobzhangtech@users.noreply.github.com>
2026-03-25 20:39:04 -0700

    Modify .gitignore, add new folders containing new files

---
+++ src/main.py
@@ -0,0 +1,25 @@
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+def menu():
+    print("Hi! I'm your stock market investment assistant!")
+    print("1. Add ticker symbol")
+    print("2. Remove ticker symbol")
+    user_input = int(input("Please enter the number of your selection: ").strip())
+
+
+def display_tickers():
+    pass
+
+
+def add_ticker(user_input):
+    pass
+
+
+def remove_ticker(user_input):
+    pass
---
+++ utils/data_fetcher.py
@@ -0,0 +1,26 @@
+import yfinance as yf
+
+# Fetch the data of the S&P 500 and NASDAQ indices
+indices = yf.download("^GSPC ^IXIC", period="1y", interval="1d")
+sorted_indices = indices.sort_index(ascending=False)
+filtered_indices = sorted_indices["Close"]
+
+selected_data = filtered_indices.iloc[0, [0, 1]]
+
+
+def fetch_ticker_info(ticker):
+    data = yf.Ticker(f"{ticker.strip().upper()}")
+    info = data.info
+    revenue_growth = info.get("revenueGrowth")  # Look for consistent top-line growth
+    earnings_growth = info.get(
+        "earningsGrowth"
+    )  # Look for consistent bottom-line growth
+    balance_sheet = data.balance_sheet
+    profit_margins = info.get("profitMargins")
+    cash_flow = data.cashflow  # Look for consistent cash flow
+    # Competitive advantage (moat)
+    operating_margins = info.get("operatingMargins")  # Look for high operating margins
+    return_on_invested_capital = info.get(
+        "returnOnInvestedCapital"
+    )  # Look for high ROIC
+    debt_to_equity = info.get("debtToEquity")  # Look for low debt to equity
