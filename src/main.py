f12db61b969c6cbb71dcff8c0c38fa824eebe680
Bob Zhang <270489537+bobzhangtech@users.noreply.github.com>
2026-03-28 12:32:15 -0700

    Modify main.py, re-add utils folder, move telegram_bot.py to utils/

--- src/main.py
+++ src/main.py
@@ -1,23 +1,17 @@
 from data_fetcher import fetch_ticker_data
-from ollama import chat
-from ollama import ChatResponse
+import ollama
 
 user_input = input("\nEnter ticker symbol: ")
 print("\nGenerating response...")
 
-response: ChatResponse = chat(
-    model="huihui_ai/gemma3-abliterated:4b",
-    messages=[
-        {
-            "role": "user",
-            "content": f"With only the following data, help me determine if this stock is a good buy or not: {(fetch_ticker_data(user_input))}. Don't say you don't know, and don't say you're not a financial advisor, just give me a solid answer in one paragraph with supporting evidence while keeping it concise and to the point. Don't ask any follow-up questions and just end it off after your answer.",
-        },
-    ],
+response = ollama.generate(
+    model="huihui_ai/qwen3.5-abliterated:2B",
+    think=True,
+    prompt=f"With only the following data, help me determine if this stock is a good buy or not: {(fetch_ticker_data(user_input))}. Don't say you don't know, and don't say you're not a financial advisor, just give me a solid answer in one paragraph with supporting evidence while keeping it concise and to the point. Only refer to the stock by its ticker symbol and not what you think its actual name is. Don't ask any follow-up questions and just end it off after your answer.",
+    keep_alive=0,
 )
 
-print("\n" + response["message"]["content"] + "\n")
-# or access fields directly from the response object
-# print(response.message.content)
+print("\n" + response["response"] + "\n")
 
 
 def main():
