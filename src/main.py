0f8e3e79e215429a85de83275e233d19e75342ce
Bob Zhang <270489537+bobzhangtech@users.noreply.github.com>
2026-03-27 23:25:54 -0700

    Modify main.py

--- src/main.py
+++ src/main.py
@@ -3,16 +3,18 @@ from ollama import chat
 from ollama import ChatResponse
 
 user_input = input("\nEnter ticker symbol: ")
+print("\nGenerating response...")
 
 response: ChatResponse = chat(
-    model="gemma3:4b",
+    model="huihui_ai/gemma3-abliterated:4b",
     messages=[
         {
             "role": "user",
-            "content": f"With only the following data, help me determine if this stock is a good buy or not: {(fetch_ticker_data(user_input))}. Don't say you don't know, and don't say you're not a financial advisor, just give me a solid answer with evidence while keeping it concise and to the point.",
+            "content": f"With only the following data, help me determine if this stock is a good buy or not: {(fetch_ticker_data(user_input))}. Don't say you don't know, and don't say you're not a financial advisor, just give me a solid answer in one paragraph with supporting evidence while keeping it concise and to the point. Don't ask any follow-up questions and just end it off after your answer.",
         },
     ],
 )
+
 print("\n" + response["message"]["content"] + "\n")
 # or access fields directly from the response object
 # print(response.message.content)
