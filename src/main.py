49da9ed7ac13c19e7865ab8e9efa1f57e4bc2fa2
Bob Zhang <270489537+bobzhangtech@users.noreply.github.com>
2026-03-28 23:27:22 -0700

    Switch to another local AI model

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ def main():
         print("\nGenerating response...")
 
         response = ollama.generate(
-            model="ministral-3:3b",
+            model="deepseek-r1:7b",
             prompt="You are a stock market investment assistant. With the provided data, help me determine if the stock/ETF/cryptocurrency is a good buy or not. Give me a definitive answer in a single paragraph with supporting evidence while keeping it concise and to the point. If any part of the data is empty or returns none, it only means the data is not documented and doesn't mean anything else. Don't ask any follow-up questions and just end it off after your answer."
             + f"{(fetch_ticker_data(user_input))}",
             keep_alive=0,
